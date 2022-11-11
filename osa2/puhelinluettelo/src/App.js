ce5a71f4eb258d5ed84f7a8de357c1ce7b1435e4
unknown <samuli.raivio98@gmail.com>
2022-11-11 12:18:16 +0200

    osan 2 puhelinluettelo tehty kohtaan step6

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -1,13 +1,66 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+import axios from 'axios'
+
+
+//Person komponentti palauttaa yhdin listan osan jossa näkyy henkilön nimi ja numero
+const Person = ({person}) => {
+  return (
+    <li>{person.name} {person.number}</li>
+  )
+}
+
+//Filter komponentti ottaa käyttäjän syötteestä merkkijonon
+const Filter = ({filterName, filterPerson}) => {
+  return(
+    <div>filter shown with <input value={filterName} onChange={filterPerson} /></div>
+  )
+}
+
+
+//PersonForm komponentti ottaa käyttäjän syötteestä nimen ja numeron
+const PersonForm = ({addPerson, newName, handleNameChange, newNumber, handleNumberChange}) => {
+  return (
+    <form onSubmit={addPerson}>
+        <div>name: <input value={newName} onChange={handleNameChange}/></div>
+        <div>number: <input value={newNumber} onChange={handleNumberChange}/></div>
+        <div><button type="submit">add</button></div>
+      </form>
+  )
+}
+
+//Persons komponentti palauttaa listan henkilöitä Part komponentissa
+//palautuksessa ensin filtteröidään käyttäjän antaman syötteen mukaan henkilöt ja lopulta mapataan ne nätisti Person komponentin mukaisiksi
+const Persons = ({persons, filterName}) => {
+
+  return (
+    <ul>
+      {persons.filter(person => person.name.toLowerCase().includes(filterName.toLocaleLowerCase())).map((person, i) => <Person key={i} person={person}/>)}
+    </ul>
+  )
+
+}
+
 
 const App = () => {
+
+
   //määritellään objektijono henkilöistä
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-1231244' }
-  ]) 
+  const [persons, setPersons] = useState([]);
+
+  //haetaan persons listaan oliot json serveriltä effect hookilla
+  useEffect(() => {
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        setPersons(response.data)
+      })
+  }, [])
+
+
   //newName on uusi nimi joka luetaan käyttäjän inputista
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+  const [filterName, setFilterName] = useState('')
 
   
   //handleNameChange tallentaa käyttäjän inputin muuttujaan newName
@@ -42,25 +95,21 @@ const App = () => {
   }
 
 
+  //filterPerson tapahtumankäsittelijä antaa filterName muuttujalle sen arvon minkä käyttäjä on syöttänyt suodatukseen
+  const filterPerson = (event) => {
+    setFilterName(event.target.value)
+    console.log(persons.filter(person => person.name.includes(filterName))) 
+  }
 
-  
-
-  
-  
-  
 
   return (
     <div>
       <h2>Phonebook</h2>
-      <form onSubmit={addPerson}>
-        <div>name: <input value={newName} onChange={handleNameChange}/></div>
-        <div>number: <input value={newNumber} onChange={handleNumberChange}/></div>
-        <div><button type="submit">add</button></div>
-      </form>
+      <Filter filterName={filterName} filterPerson={filterPerson} />
+      <h2>add a new</h2>
+      <PersonForm addPerson={addPerson} newName={newName} handleNameChange={handleNameChange} newNumber={newNumber} handleNumberChange={handleNumberChange}/>
       <h2>Numbers</h2>
-      <ul>
-        {persons.map((person, i) => <li key={i}>{person.name} {person.number}</li>)}
-      </ul>
+      <Persons persons={persons} filterName={filterName}/>
     </div>
   )
 
