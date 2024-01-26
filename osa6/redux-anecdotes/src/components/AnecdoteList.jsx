12b34e69ce26f1aad166620b989ab64df402c0aa
unknown <samuli.raivio98@gmail.com>
2024-01-26 18:41:55 +0200

    Exercises 6.3 - 6.8 done

---
+++ osa6/redux-anecdotes/src/components/AnecdoteList.jsx
@@ -0,0 +1,34 @@
+import { useDispatch, useSelector } from "react-redux";
+import { voteAnecdote } from "../reducers/anecdoteReducer";
+
+const Anecdote = ({ anecdote, handleClick }) => {
+  return (
+    <div key={anecdote.id}>
+      <div>{anecdote.content}</div>
+      <div>
+        has {anecdote.votes}
+        <button onClick={handleClick}>vote</button>
+      </div>
+    </div>
+  );
+};
+
+const Anecdotes = () => {
+  const dispatch = useDispatch();
+  const anecdotes = useSelector((state) => state);
+  const sortedBlogsByLikes = anecdotes.sort((a, b) => b.votes - a.votes);
+
+  return (
+    <div>
+      {anecdotes.map((anecdote) => (
+        <Anecdote
+          key={anecdote.id}
+          anecdote={anecdote}
+          handleClick={() => dispatch(voteAnecdote(anecdote.id))}
+        />
+      ))}
+    </div>
+  );
+};
+
+export default Anecdotes;
