3d5f7cf3fff27b0f15083797647a2669744cc8e6
unknown <samuli.raivio98@gmail.com>
2024-01-26 17:21:40 +0200

    Exercises 6.1 and 6.2 done

---
+++ osa6/unicafe-redux/src/reducer.test.js
@@ -0,0 +1,125 @@
+import deepFreeze from "deep-freeze";
+import counterReducer from "./reducer";
+import { createStore } from "redux";
+
+describe("unicafe reducer", () => {
+  const initialState = {
+    good: 0,
+    ok: 0,
+    bad: 0,
+  };
+
+  test("should return a proper initial state when called with undefined state", () => {
+    const state = {};
+    const action = {
+      type: "DO_NOTHING",
+    };
+
+    const newState = counterReducer(undefined, action);
+    expect(newState).toEqual(initialState);
+  });
+
+  test("good is incremented", () => {
+    const action = {
+      type: "GOOD",
+    };
+    const state = initialState;
+
+    deepFreeze(state);
+    const newState = counterReducer(state, action);
+    expect(newState).toEqual({
+      good: 1,
+      ok: 0,
+      bad: 0,
+    });
+  });
+
+  test("ok is incremented", () => {
+    const action = {
+      type: "OK",
+    };
+    const state = initialState;
+
+    deepFreeze(state);
+    const newState = counterReducer(state, action);
+    expect(newState).toEqual({
+      good: 0,
+      ok: 1,
+      bad: 0,
+    });
+  });
+
+  test("bad is incremented", () => {
+    const action = {
+      type: "BAD",
+    };
+    const state = initialState;
+
+    deepFreeze(state);
+    const newState = counterReducer(state, action);
+    expect(newState).toEqual({
+      good: 0,
+      ok: 0,
+      bad: 1,
+    });
+  });
+
+  test("good, ok and bad are incremented", () => {
+    const store = createStore(counterReducer);
+
+    const actionGood = {
+      type: "GOOD",
+    };
+    const actionOk = {
+      type: "OK",
+    };
+    const actionBad = {
+      type: "BAD",
+    };
+    const state = initialState;
+
+    deepFreeze(state);
+    store.dispatch(actionGood);
+    store.dispatch(actionOk);
+    store.dispatch(actionBad);
+    expect(store.getState()).toEqual({
+      good: 1,
+      ok: 1,
+      bad: 1,
+    });
+  });
+
+  test("zero resets", () => {
+    const store = createStore(counterReducer);
+
+    const actionGood = {
+      type: "GOOD",
+    };
+    const actionOk = {
+      type: "OK",
+    };
+    const actionBad = {
+      type: "BAD",
+    };
+    const actionZero = {
+      type: "ZERO",
+    };
+    const state = initialState;
+
+    deepFreeze(state);
+    store.dispatch(actionGood);
+    store.dispatch(actionOk);
+    store.dispatch(actionBad);
+    expect(store.getState()).toEqual({
+      good: 1,
+      ok: 1,
+      bad: 1,
+    });
+    store.dispatch(actionZero);
+    expect(store.getState()).toEqual({
+      good: 0,
+      ok: 0,
+      bad: 0,
+    });
+  });
+});
